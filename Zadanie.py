00f251308a37772fcba83d1c90cd6954dee12197
W4ldek <46499524+W4ldek@users.noreply.github.com>
2019-01-12 13:34:47 +0100

    Update Zadanie.py

--- Zadanie.py
+++ Zadanie.py
@@ -30,6 +30,15 @@ def r_M(m_M,m_E,d):
     r=(m_E/(m_M+m_E))*d
     return r
 
+def RK(y,t,dt,derivative):
+    k1=derivative(y,t)*dt
+    k2=derivative(y+k1/2,t+dt/2)*dt
+    k3=derivative(y+k2/2,t+dt/2)*dt
+    k4=derivative(y+k3,t+dt)*dt
+    dy=(1/6.)*(k1+2*k2+2*k3+k4)
+    y_next=y+dy
+    return y_next
+
 ro=2*np.pi/T
 #R orbity 6800 km
 #y=np.zeros([n,4])
